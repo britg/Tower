ce3d7b386b35d399f38e58ed13ed885658cfc0a8
britg <brit@britg.com>
2015-06-10 13:57:41 -0500

    initial commit

---
+++ Assets/Scripts/Game/CameraBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+  public int targetFrameRate = 60;
+
+	// Use this for initialization
+	void Start () {
+    Application.targetFrameRate = targetFrameRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
