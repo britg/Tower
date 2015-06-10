9982439ea12d8c7e64f18a1f80b5315d134ce99b
britg <brit@britg.com>
2015-06-10 16:08:56 -0500

    rebuild solution

--- Assets/Scripts/Game/FollowPlayerBehaviour.cs
+++ Assets/Scripts/Game/FollowPlayerBehaviour.cs
@@ -7,11 +7,18 @@ public class FollowPlayerBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	  
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	void LateUpdate () {
+		var pos = transform.position;
+		pos.x = player.transform.position.x;
+		pos.z = player.transform.position.z;
+		transform.position = pos;
 	}
 }
